12d6d1c68d0378dab66386f22a838ba7964ee3b0
Nicholas-Barbosa <nicholas.henrique0604@hotmail.com>
2021-01-14 18:21:48 -0300

    Banner added

---
+++ src/main/java/com/pipeline/path/SfPathHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.pipeline.path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfPathHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfPathHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pipeline/path/SfPathHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipeline.path;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfPathHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
